af2fd833198429bd6ada5ef4cf726c195dc016af
vuongvinhvien <vuongvinhvien@gmail.com>
2018-10-31 09:56:04 +0700

    change

--- ChatBoxSellApp.Web/Models/IdentityModels.cs
+++ ChatBoxSellApp.Web/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace ChatBoxSellApp.Web.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("DataChatBox", throwIfV1Schema: false)
         {
         }
 
